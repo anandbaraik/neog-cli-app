8867ff9dcb7f93a32e95df443c538805453366a7
Anand Baraik <anandbaraik2014@gmail.com>
2022-07-26 16:48:30 +0000

    ex03: welcome your user

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let readlineSync = require('readline-sync');
 
 let name = readlineSync.question('May i have your name..?');
-console.log(`Hello ${name}`);
+console.log(`Welcome ${name}`);
+console.log('Welcome '+ name);
