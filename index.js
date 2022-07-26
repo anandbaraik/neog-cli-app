80185b0f08e639895577da411c69fe8552877b97
Anand Baraik <anandbaraik2014@gmail.com>
2022-07-26 17:14:23 +0000

    ex:inverted star

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let readlineSync = require('readline-sync');
 let num = readlineSync.questionInt('Enter max number to be printed as star..?');
 
 // method 1
-for(let i = 1; i<= num; i++) {
+for(let i = num; i >= 1; i--) {
   let star = '*'.repeat(i);
   console.log(`${star}\n`);
 }
 
 // method 2
-for(let i = 1; i <= num; i++) {
+for(let i = num; i >= 1; i--) {
   let pattern = "";
   for(let j = 1; j <= i; j++) {
     pattern += "*";
